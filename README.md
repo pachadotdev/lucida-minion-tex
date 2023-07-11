35d9f28a7d7f57d67fb52ba8fefe5e57e2babd4f
Mauricio Vargas <mavargas11@uc.cl>
2023-07-10 22:25:40 -0400

    add examples

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# install-minion-pro-and-lucida
-Install Minion Pro and Lucida for TeX
+# Install Minion Pro and Lucida for TeX
+
+## About
+
+This repository provides a script to install the Lucida and Minion Pro fonts for TeX. Here's a [pdf] created from a [tex] file that uses Computer Modern and here's another [pdf] created from modification of the [tex] file using Lucida and Minion Pro.
+
+Lucida is a commercial font that can be purchased from [TUG](https://www.tug.org/store/lucida/index.html). After purchasing the font, you will receive a zip files with the fonts and the macros (lucida-type1.zip and lucimacros.zip).
+
+Minion Pro comes with Adobe Illustrator or can be purchased by separate from [Adobe](https://fonts.adobe.com/fonts/minion).
+
+## Instructions
+
+### General
+
+Clone this repository:
+
+```bash
+git clone https://github.com/pachadotdev/lucida-minion-tex.git
+```
+
+Put the corresponding zip files for Lucida in the root folder of this repository.
+
+If you don't plan to install Minion Pro, skip the rest of this section.
+
+Create the `minion` folder in the root of this repository and put the Minion Pro fonts in it. This corresponds to the next files:
+
+```
+MinionPro-BoldCnIt.otf
+MinionPro-BoldCn.otf
+MinionPro-BoldIt.otf
+MinionPro-Bold.otf
+MinionPro-It.otf
+MinionPro-MediumIt.otf
+MinionPro-Medium.otf
+MinionPro-Regular.otf
+MinionPro-SemiboldIt.otf
+MinionPro-Semibold.otf
+```
+
+The script in the next section will convert the fonts and put them in the `minion-nofont` directory, which contains the metrics and other files required for TeX.
+
+### Linux
+
+Run the script `install.sh` and select the available options:
+
+1. Lucida
+2. Minion Pro
+3. Both
+
+```bash
+sudo bash -c "$(wget -qO- https://github.com/pachadotdev/lucida-minion-tex/blob/main/install.sh)"
+```
+
+This should also work with Mac, but you'll need to install `lcdf-typetools` or any other tool that provides `cfftot1` and `t1dotlessj`, which are only required for Minion Pro.
+
+### Windows
+
+1. Create the `C:\texfonts` folder.
+2. Unzip the `lucida-type1.zip` and `lucimacros.zip` files into the folder created in the previous step.
+3. Go to the Miktex configuration (Start > Programs > MiKTeX > Maintenance).
+4. Add the folder created in the first step (see image 1).
+5. Update the FNDB and formats (see image 2) don't forget to click "apply" and then "ok".
+
+To convert the font from OTF to TeX format you need to run something of the form:
+
+```bash
+@echo off
+
+echo "Creating PostScript fonts ..."
+
+for %%i in (minion/*.otf) do cfftot1    minion/otf/%%i minion-nofont/pfb/%%~ni.pfb
+for %%i in (minion/*.otf) do t1dotlessj minion/pfb/%%~ni.pfb minion-nofont/pfb/%%~niLCDFJ.pfb
+```
+
+I don't use Windows, so this is what my imagination says is the adaption from the Linux script.
--- install.sh
+++ install.sh
@@ -1,32 +1,67 @@
 #!/bin/bash
 
-# set 
+# what to install
+
+# prompt with 3 options
+echo "Which font do you want to install?"
+echo "2. Lucida"
+echo "1. Minion Pro"
+echo "3. Both"
+read -p "Enter your choice (1, 2, or 3): " choice
+
+# setup 
 
 # if ~/.TinyTeX/texmf-local exists, do nothing
 # otherwise, install TinyTeX
 if [ ! -d ~/.TinyTeX/texmf-local ]; then
-    wget -qO- "https://yihui.org/tinytex/install-bin-unix.sh" | sh
+  echo "Installing TinyTeX..."
+  wget -qO- "https://yihui.org/tinytex/install-bin-unix.sh" | sh
 fi
 
-unzip -o lucida-type1.zip -d ~/.TinyTeX/texmf-local
-unzip -o lucimacros.zip -d ~/.TinyTeX/texmf-local
+# if choice 1 or 3, install Lucida
+if [ $choice = 1 ] || [ $choice = 3 ]; then
+  echo "Installing Lucida..."
+  unzip -o lucida-type1.zip -d ~/.TinyTeX/texmf-local
+  unzip -o lucimacros.zip -d ~/.TinyTeX/texmf-local
+  mktexlsr
+  updmap-sys --enable Map=lucida.map
+  updmap-sys
+fi
 
-mkdir -p ~/.TinyTeX/texmf-local/fonts/type1/adobe/MinionPro
-unzip -o minion.zip
-cp minion/pfb/*.pfb ~/.TinyTeX/texmf-local/fonts/type1/adobe/MinionPro
-unzip -o minion/metrics-base.zip -d ~/.TinyTeX/texmf-local/
-unzip -o minion/enc-2.000.zip -d ~/.TinyTeX/texmf-local/
+# if choice 2 or 3, install Minion Pro
+if [ $choice = 2 ] || [ $choice = 3 ]; then
+  echo "Installing Minion Pro..."
 
-mktexlsr
-updmap-sys --enable Map=lucida.map
-updmap-sys --enable Map=MinionPro.map
-updmap-sys
+  # if apt exists, then run sudo update
+  if command -v apt &> /dev/null
+  then
+    sudo apt update
 
-# \usepackage[openg]{MinionPro} won’t show the letter “g” and \ell won’t work 
-# with this typography. Here is the solution.
+    # if cfftot1 does not exist, install it
+    if ! command -v cfftot1 &> /dev/null
+    then
+      sudo apt install lcdf-typetools
+    fi
+  fi
 
-# replace "/afii61289" with "/uni2113" in .TinyTeX/texmf-local/fonts/enc/dvips/MinionPro/base-MinionPro-ab.enc
-sed -i 's/\/afii61289/\/uni2113/g' ~/.TinyTeX/texmf-local/fonts/enc/dvips/MinionPro/base-MinionPro-ab.enc
-
-# also replace "/afii10069.ital" with "/uni0434.ital" in the same file
-sed -i 's/\/afii10069.ital/\/uni0434.ital/g' ~/.TinyTeX/texmf-local/fonts/enc/dvips/MinionPro/base-MinionPro-ab.enc
+  mkdir -p ~/.TinyTeX/texmf-local/fonts/type1/adobe/MinionPro
+  for font in minion/*.otf
+  do
+    base=$(basename "$font" .otf)
+    cfftot1 "$font" "minion-nofont/pfb/$base.pfb"
+    t1dotlessj "minion-nofont/pfb/$base.pfb" "minion-nofont/pfb/${base}LCDFJ.pfb"
+  done
+  cp minion-nofont/pfb/*.pfb ~/.TinyTeX/texmf-local/fonts/type1/adobe/MinionPro
+  unzip -o minion-nofont/metrics-base.zip -d ~/.TinyTeX/texmf-local/
+  unzip -o minion-nofont/enc-2.000.zip -d ~/.TinyTeX/texmf-local/
+  mktexlsr
+  updmap-sys --enable Map=MinionPro.map
+  updmap-sys
+  
+  # \usepackage[openg]{MinionPro} won’t show the letter “g” and \ell won’t work 
+  # with this typography. Here is the solution.
+  # replace "/afii61289" with "/uni2113" in .TinyTeX/texmf-local/fonts/enc/dvips/MinionPro/base-MinionPro-ab.enc
+  sed -i 's/\/afii61289/\/uni2113/g' ~/.TinyTeX/texmf-local/fonts/enc/dvips/MinionPro/base-MinionPro-ab.enc
+  # also replace "/afii10069.ital" with "/uni0434.ital" in the same file
+  sed -i 's/\/afii10069.ital/\/uni0434.ital/g' ~/.TinyTeX/texmf-local/fonts/enc/dvips/MinionPro/base-MinionPro-ab.enc
+fi
